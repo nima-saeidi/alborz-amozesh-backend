f445fb33a90ee6da63d1a57f0221278530d72c07
nima-saeidi <131960679+nima-saeidi@users.noreply.github.com>
2025-11-08 17:10:25 +0330

    Allow all hosts in ALLOWED_HOSTS setting

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','*']
 AUTH_USER_MODEL = 'users.User'
 
 
